1d3ca21e3da14416f2020418b5f00736fbe7672f
SANATH S CHINIVAR <vvce23ise0098@vvce.ac.in>
2026-02-28 11:52:38 +0530

    Add AddTwoNumbers.java to calculate sum of two integers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Calculate the sum and store it in a third variable
+        int sum = num1 + num2;
+        
+        // Print the result to the console
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
